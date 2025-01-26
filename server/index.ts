d014f3034e64dee150a30a2aaad7007a8e77d0ea
Krishna Saxena <rio17105@gmail.com>
2025-01-27 02:06:28 +0530

    init

---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import { createHTTPServer } from "@trpc/server/adapters/standalone";
+import { router } from "./trpc";
+const appRouter = router({
+  // ...
+});
+// Export type router type signature,
+// NOT the router itself.
+export type AppRouter = typeof appRouter;
+
+const server = createHTTPServer({
+  router: appRouter,
+});
+
+server.listen(3000);
